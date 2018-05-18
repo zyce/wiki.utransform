13bb4e41d8379554b1a16b5d7354276bde616ff0
durbanitas <durbanitas@gmail.com>
2018-05-18 18:19:04 +0200

    MediaWiki Short URL

--- LocalSettings.php
+++ LocalSettings.php
@@ -23,11 +23,12 @@ $wgMetaNamespace = "Wiki_name";
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
-## For more information on customizing the URLs
-## (like /w/index.php/Page_title to /wiki/Page_title) please see:
-## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = '' == '' ? "" : "/";
+## For more information on customizing the URLs please see:
+## http://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath = "";
 $wgScriptExtension = ".php";
+$wgArticlePath = "/wiki/$1";
+$wgUsePathInfo = true;
 
 ## The protocol and server name to use in fully-qualified URLs
 #$wgServer = "http://wiki.utransform.de";
@@ -130,3 +131,7 @@ session_save_path(dirname( __FILE__ ) . '/tmp');
 # Benutzerregistrierung deaktivieren
 $wgGroupPermissions[‚*‘][‚createaccount‘] = false;
 $egChameleonLayoutFile= __DIR__ . '/skins/chameleon/layouts/navhead.xml';
+## To enable image uploads, make sure the 'images' directory
+## is writable, then set this to true:
+$wgEnableUploads  = true;
+$wgGenerateThumbnailOnParse = false;
